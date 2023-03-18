3ce0d35b26c9f486954d7a1565dec66a35589460
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-18 14:52:56 +0900

    validation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from '@src/common/all-exceptions.filter';
 import { HttpLoggingInterceptor } from '@src/common/http-logging.interceptor';
 import { AppModule } from '@src/app.module';
@@ -10,6 +11,14 @@ async function bootstrap() {
     app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
     app.useGlobalInterceptors(new HttpLoggingInterceptor());
+
+    /**
+     * whitelist - https://seungtaek-overflow.tistory.com/13
+     * enableImplicitConversion - https://gongmeda.tistory.com/64
+     */
+    app.useGlobalPipes(
+        new ValidationPipe({ transform: true, whitelist: true, transformOptions: { enableImplicitConversion: true }, disableErrorMessages: false }),
+    );
 
     await app.listen(3000);
 }
