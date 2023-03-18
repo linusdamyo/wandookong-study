eac7c982f97d13eb82d7e3d567fb13036a2c9ef8
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-18 14:21:02 +0900

    eslint, tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,25 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: 'tsconfig.json',
-        tsconfigRootDir: __dirname,
         sourceType: 'module',
     },
     plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
     root: true,
     env: {
         node: true,
         jest: true,
     },
-    ignorePatterns: ['.eslintrc.js'],
+    ignorePatterns: ['.eslintrc.js', './dist/**'],
     rules: {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'prettier/prettier': 'error',
+        'no-return-await': 'error',
+        'object-shorthand': 'error',
+        quotes: ['error', 'single', { avoidEscape: true }],
     },
 };
