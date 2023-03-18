b7285de50a303d94b38dc7d27c7b1ebfd2787cb8
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-18 14:28:04 +0900

    exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { AllExceptionsFilter } from '@src/common/all-exceptions.filter';
 import { AppModule } from '@src/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const httpAdapter = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
     await app.listen(3000);
 }
 bootstrap();
