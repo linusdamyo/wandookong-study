2d951178fe1611adb841e0fb9df83e9f09d4f6eb
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-25 17:40:30 +0900

    AuthUser decorator 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, UseGuards, Get } from '@nestjs/common';
 import { UsersService } from '@src/users/users.service';
+import { JwtGuard } from '@src/auth/jwt/jwt.guard';
 import { AuthUser, AuthUserType } from '@src/common/decorators/auth-user.decorator';
 import { UsersMeResponseDto } from '@src/users/dto/users-me.dto';
 
+@UseGuards(JwtGuard)
 @Controller('/users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
