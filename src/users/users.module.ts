1f71ead62b97051e2c2ab81d97215b8a8c8c63de
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-25 17:00:20 +0900

    user repository 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from '@src/users/users.service';
 import { UserEntity } from '@src/users/entities/user.entity';
+import { UsersRepository } from '@src/users/users.repository';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserEntity])],
-    providers: [UsersService],
+    providers: [UsersService, UsersRepository],
     exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '@src/users/users.repository';
 
 @Injectable()
 export class UsersService {
-    async getUser() {
-        return { id: 1 };
+    constructor(private readonly usersRepository: UsersRepository) {}
+
+    async getUser(userId: number) {
+        return this.usersRepository.findOne(userId);
     }
 }
