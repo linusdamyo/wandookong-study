ea2cb2d59f356bb3b4c054462441ef7821042ea4
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-25 15:45:15 +0900

    signIn - accessToken 반환 처리 테스트 추가

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -19,7 +19,9 @@ describe('AuthService', () => {
         service = module.get<AuthService>(AuthService);
     });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
+    it('signIn() - accessToken 반환', async () => {
+        const result = await service.signIn({ email: 'iu@wandookong.com', password: 'iuiuiu' });
+
+        expect(result.accessToken).toBeDefined();
     });
 });
