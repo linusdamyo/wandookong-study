cebe2c0f61a6188e91df4a7e82edb9ba0ff130b6
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-25 16:42:41 +0900

    user entity 테스트

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { UsersService } from '@src/users/users.service';
+import { UsersModule } from '@src/users/users.module';
+import { UserEntity } from '@src/users/entities/user.entity';
 
 describe('UsersService', () => {
     let service: UsersService;
+    let dataSource: DataSource;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UsersService],
+            imports: [
+                ConfigModule.forRoot({
+                    envFilePath: `src/config/.${process.env.NODE_ENV}.env`,
+                }),
+                TypeOrmModule.forRootAsync({
+                    imports: [ConfigModule],
+                    inject: [ConfigService],
+                    useFactory: (configService: ConfigService) => ({
+                        type: 'mysql',
+                        host: configService.get('DB_HOST'),
+                        port: +configService.get('DB_PORT'),
+                        username: configService.get('DB_USERNAME'),
+                        password: configService.get('DB_PASSWORD'),
+                        database: configService.get('DB_DATABASE'),
+                        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                        synchronize: true,
+                        autoLoadEntities: true,
+                        logging: false,
+                        charset: 'utf8mb4',
+                    }),
+                }),
+                UsersModule,
+            ],
         }).compile();
 
         service = module.get<UsersService>(UsersService);
+        dataSource = module.get<DataSource>(DataSource);
     });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
+    it('load user entity', async () => {
+        const users = await dataSource.getRepository(UserEntity).find();
+
+        expect(users).toEqual([]);
+    });
+
+    afterAll(async () => {
+        await dataSource.destroy();
     });
 });
