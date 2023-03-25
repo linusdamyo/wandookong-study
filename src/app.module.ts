15ac09009443ecf0d137d56f4eb31209b7be9994
Geunsu Ryu <linusdamyo@gmail.com>
2023-03-25 15:25:17 +0900

    config - dotenv 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
+import Joi from 'joi';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `${__dirname}/config/.${process.env.NODE_ENV}.env`,
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
