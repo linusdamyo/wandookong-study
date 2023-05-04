6f5a529c1ac4579139eb51c2e3ebed47a04c8c54
Geunsu Ryu <linusdamyo@gmail.com>
2023-05-04 21:59:33 +0900

    signin dto validation 추가
    - ValidationPipe whitelist: true 이면, request에 validation은 필수!

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class SignInDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
 
