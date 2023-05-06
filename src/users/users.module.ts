7137d32ed066f21d7a22e5f92b605ffcab8e6d75
Geunsu Ryu <linusdamyo@gmail.com>
2023-05-06 14:00:26 +0900

    TypeOrmModule.forFeature 제거

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from '@src/users/users.service';
-import { UserEntity } from '@src/users/entities/user.entity';
 import { UsersRepository } from '@src/users/users.repository';
 import { UsersController } from '@src/users/users.controller';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserEntity])],
     controllers: [UsersController],
     providers: [UsersService, UsersRepository],
     exports: [UsersService],
